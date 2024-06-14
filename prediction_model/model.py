7a515f6805964c4f348464f81e409eae9077521f
yottso <illya.stavsky.2004@gmail.com>
2024-06-14 15:13:07 +0300

    added test tkinter app

--- prediction_model/model.py
+++ prediction_model/model.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import datetime
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+import pickle
 from xgboost import XGBRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,7 +14,6 @@ def replace_open_dates(df):
     for i in range(len(df)):
         df['Open Date'][i] = (today - df['Open Date'][i].date()).days
     return df
-
 
 data_train = pd.read_csv("datasets/train.csv")
 data_test = pd.read_csv("datasets/test.csv")
@@ -34,32 +35,55 @@ X_test = data_test[["Open Date", "City", "Type", "Points1", "Points2", "Points3"
                       "Points7", "Points9", "Points11", "Points14", "Points21", "Points24", "Points26",
                       "Points31", "Points37"]]
 
-
 X_all = pd.concat([X_train, X_test])
 
-label_encoder = LabelEncoder()
-X_all['City'] = label_encoder.fit_transform(X_all['City'])
-X_all['Type'] = label_encoder.fit_transform(X_all['Type'])
+label_encoder_city = LabelEncoder()
+label_encoder_type = LabelEncoder()
+X_all['City'] = label_encoder_city.fit_transform(X_all['City'])
+X_all['Type'] = label_encoder_type.fit_transform(X_all['Type'])
 
-X_all = pd.get_dummies(X_all, columns=["City", "Type"])
 
 X_train = X_all[:len(X_train)]
 X_test = X_all[len(X_train):]
+
+correlations = X_train.corrwith(data_train['revenue'])
+correlations_df = pd.DataFrame({'Correlation': correlations})
+correlations_df = correlations_df.sort_values(by='Correlation', ascending=False)
+plt.figure(figsize=(12, 10))
+sns.heatmap(correlations_df.T, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title("Корреляция признаков с revenue")
+plt.show()
 
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+param_grid = {
+    'n_estimators': [500, 1000, 1500],
+    'max_depth': [5, 7, 9],
+    'learning_rate': [0.01, 0.05, 0.1],
+    'gamma': [0.0, 0.1, 0.2]
+}
+
 model = XGBRegressor(objective='reg:squarederror',
                     n_estimators=1000,
                     max_depth=7,
-                    learning_rate=0.04,
+                    learning_rate=0.03,
                     gamma=0.1)
 
+# grid_search = GridSearchCV(estimator=model, param_grid=param_grid, scoring='neg_mean_squared_error', cv=5)
+# grid_search.fit(X_train, y_train)
+#
+# best_params = grid_search.best_params_
+# model = XGBRegressor(objective='reg:squarederror', **best_params)
+
 model.fit(X_train, y_train,  eval_set=[(X_train, y_train)],
-          eval_metric='rmse', verbose=True, early_stopping_rounds=7)
+          eval_metric='rmse', verbose=True, early_stopping_rounds=4)
 
 evals_result = model.evals_result()
+
+with open('model.pkl', 'wb') as file:
+    pickle.dump([model, scaler, label_encoder_city, label_encoder_type], file)
 
 predictions = model.predict(X_test)
 predictions = predictions.astype(int)
@@ -70,12 +94,6 @@ mae = mean_absolute_error(y_test, predictions)
 
 print(f"Среднеквадратичная ошибка (MSE): {mse}")
 print(f"Средняя абсолютная ошибка (MAE): {mae}")
-
-correlation_matrix = X_train.corr()
-plt.figure(figsize=(12, 10))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-plt.title("Матрица корреляции")
-plt.show()
 
 plt.figure(figsize=(10, 6))
 plt.subplot(2, 1, 1)
---
+++ test.py
@@ -0,0 +1,50 @@
+import pickle
+import pandas as pd
+import datetime
+import tkinter as tk
+with open('prediction_model/model.pkl', 'rb') as file:
+    loaded_model, scaler, label_encoder_city, label_encoder_type = pickle.load(file)
+
+def replace_open_dates(df):
+    df['Open Date'] = pd.to_datetime(df['Open Date'], format='%m/%d/%Y')
+    today = datetime.date.today()
+    for i in range(len(df)):
+        df['Open Date'][i] = (today - df['Open Date'][i].date()).days
+    return df['Open Date'].values[0]
+
+def make_prediction():
+    data = pd.DataFrame({
+        'Open Date': ['06/16/2006'],
+        'City': ['Ankara'],
+        'Type': ['Food Casual'],
+        'Points1': [2],
+        'Points2': [3],
+        'Points3': [0],
+        'Points5': [0],
+        'Points6': [0],
+        'Points7': [0],
+        'Points9': [0],
+        'Points11': [2],
+        'Points14': [0],
+        'Points21': [0],
+        'Points24': [0],
+        'Points26': [0],
+        'Points31': [0],
+        'Points37': [0]
+    }, index=[0])
+
+    data['Open Date'] = replace_open_dates(data)
+    data['City'] = label_encoder_city.transform(data['City'])
+    data['Type'] = label_encoder_type.transform(data['Type'])
+    data = scaler.transform(data)
+    predictions = loaded_model.predict(data)
+    predictions = predictions.astype(int)
+    print(predictions)
+
+root = tk.Tk()
+root.title("Приложение для предсказаний")
+
+predict_button = tk.Button(root, text="Сделать предсказание", command=make_prediction)
+predict_button.pack()
+
+root.mainloop()
